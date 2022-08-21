c767d2ac9b252eac599c87a746911b9ca991c625
ravish0007 <ravish.nfs@gmail.com>
2022-08-21 09:30:12 +0530

    renaming get-sicp-textbook.py to 1-get-sicp-textbook.py

--- get-sicp-textbook.py
+++
@@ -1,9 +0,0 @@
-
-SICP_TEXTBOOK_URL = 'https://ocw.mit.edu/courses/6-001-structure-and-interpretation-of-computer-programs-spring-2005/c975ee8b62d01f5edb5ed01cd99e08d1_SICP_fulltext.zip'
-
-import requests
-request = requests.get(SICP_TEXTBOOK_URL)
-
-with open('SICP_FULLTEXT.zip', 'wb') as file:
-    file.write(request.content)
-
